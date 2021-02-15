803a432cd8a89ed2dd84a76b64bffa0501ec2d69
Riley Slayden <slaydenriley@gmail.com>
2021-02-14 23:48:24 -0700

    Fixing cookies

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,6 @@
-Rails.application.config.session_store :cookie_store, {
-  :key => '_slayden-portfolio',
-  :domain => :all,
-  :same_site => :none,
-  :secure => :true,
-  :tld_length => 2
-}
+if Rails.env.production?
+    Rails.application.config.session_store :cookie_store, :key => '_frontend', :domain => :all, :same_site => :none,
+      :secure => :true
+  else
+    Rails.application.config.session_store :cookie_store, key: '_frontend'
+  end
