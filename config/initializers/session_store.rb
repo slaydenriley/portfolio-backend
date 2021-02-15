32513cab8e69a3057a2a7cc48a13dcb82df2b863
Riley Slayden <slaydenriley@gmail.com>
2021-02-15 07:55:12 -0700

    Editing cookies

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 if Rails.env.production?
     Rails.application.config.session_store :cookie_store, :key => '_frontend', :domain => :all, :same_site => :none,
-      :secure => :true
+      :secure => :true, :tld_length => 2
   else
     Rails.application.config.session_store :cookie_store, key: '_frontend'
   end
