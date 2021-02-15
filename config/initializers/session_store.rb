4b86e68fade7b4a3a86dce955e5c70b022f0067d
Riley Slayden <slaydenriley@gmail.com>
2021-02-14 21:34:16 -0700

    configuring cookies

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
-    Rails.application.config.session_store :cookie_store, :key => '_frontend', domain: 'https://slayden-frontend.herokuapp.com/'
+    Rails.application.config.session_store :cookie_store, :key => '_frontend', :domain => :all
   else
     Rails.application.config.session_store :cookie_store, key: '_frontend'
   end
