f3a499eed2f60f0d217565a38034c20c807464ee
Riley Slayden <slaydenriley@gmail.com>
2021-02-15 08:56:46 -0700

    Updating seeds and login error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,7 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    rescue ActiveRecord::RecordNotFound
   end
 
   def authorized_user?
