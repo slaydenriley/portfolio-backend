b8336f21525d6cf5e13a060dac70f47f8e5dacc8
Riley Slayden <slaydenriley@gmail.com>
2021-02-14 21:27:28 -0700

    Cookie store updates for heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
   helper_method :login!, :logged_in?, :current_user, :authorized_user?, :logout!, :set_user
 
-  before_action -> { 1e7.to_i.times {} if Rails.env == 'development' }
-
   def login!
     session[:user_id] = @user.id
   end
