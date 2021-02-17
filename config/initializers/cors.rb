ed20b08715bee4bb29d356ddadc000f227774367
Riley Slayden <slaydenriley@gmail.com>
2021-02-17 14:09:52 -0700

    Configured cors to allow new domain

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://slayden-frontend.herokuapp.com'
+    origins 'https://slayden-frontend.herokuapp.com', 'www.rileyslayden.com'
 
     resource '*',
       headers: :any,
