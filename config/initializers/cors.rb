d75b44a18d681fdb9fdc319a74510bebec50d88c
Riley Slayden <slaydenriley@gmail.com>
2023-03-20 20:40:55 -0500

    update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://slayden-frontend.herokuapp.com', 'www.rileyslayden.com'
+    origins '*'
 
     resource '*',
       headers: :any,
