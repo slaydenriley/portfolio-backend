c816ca4fd548e4a77d6843920eae44345f18d220
Riley Slayden <slaydenriley@gmail.com>
2023-03-20 20:42:05 -0500

    update cors again

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://slayden-frontend.herokuapp.com', 'www.rileyslayden.com', 'https://rileyslayden.netlify.app'
 
     resource '*',
       headers: :any,
